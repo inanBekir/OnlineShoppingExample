c565645407082a73283cc21c2b5d0cba822cd8dc
Bekir <bbekirinan@gmail.com>
2018-07-09 22:19:31 +0300

    Project created version 0.0.0

---
+++ Product.Online-Marketting/Entity/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Product.Online_Marketting.Entity
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Category Description")]
+        public string Description { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Product.Online-Marketting/Entity/DataInitializer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Product.Online_Marketting.Entity
+{
+    public class DataInitializer:DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            var kategoriler = new List<Category>()
+            {
+                new Category(){Name="Camera",Description="Camera Products"},
+                new Category(){Name="Computer",Description="Computer Products"},
+                new Category(){Name="Electronics Devices",Description="Electronics Devices Products"},
+                new Category(){Name="Phones",Description="Phones Products"},
+                new Category(){Name="Smart Home Devices",Description="Smart Home Devices Products"}
+            };
+
+            foreach (var kategori in kategoriler)
+            {
+                context.Categories.Add(kategori);
+            }
+            context.SaveChanges();
+
+            var urunler = new List<Product>()
+            {
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=false,CategoryId=1,IsHome=false,Image="2.jpg"},
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=true,CategoryId=1,IsHome=true,Image="1.jpg"},
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=false,CategoryId=1,Image="3.jpg"},
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=true,CategoryId=1,IsHome=false,Image="4.jpg"},
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=false,CategoryId=1,Image="5.jpg"},
+
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=false,CategoryId=2,IsHome=true,Image="2.jpg"},
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=true,CategoryId=3,Image="3.jpg"},
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=true,CategoryId=2,IsHome=true,Image="1.jpg"},
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=true,CategoryId=4,Image="4.jpg"},
+                new Product(){Name="Canon Eos 1200D",Description="Beatiful",Price=250,Stock=150,IsApproved=true,CategoryId=2,IsHome=true,Image="5.jpg"}
+            };
+
+            foreach (var urun in urunler)
+            {
+                context.Products.Add(urun);
+            }
+            context.SaveChanges();
+            base.Seed(context);
+
+        }
+    }
+}
